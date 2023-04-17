578572e45c072a900abd5e6583879e579e1f2f4b
gabrielbrtech <gabrielcamurca@brtech.dev>
2023-04-16 21:16:08 -0300

    Include update in notes table

---
+++ database/migrations/2023_04_16_225659_add_color_to_notes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            $table->string('color')->default('#000');
+            $table->string('background_color')->default('#fff');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+        });
+    }
+};
