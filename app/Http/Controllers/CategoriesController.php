40f4a0d740c19eba0c44bb710be880ed8a5ee7b7
gabrielbrtech <gabrielcamurca@brtech.dev>
2023-05-29 21:59:50 -0300

    Create categories model e controller

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categories;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoriesController extends Controller
+{
+    public function create(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string',
+                'user_id' => 'required|numeric'
+            ],
+            $messages = [
+                'name.required' => 'Você precisa preeencher o nome da categoria.'
+            ]
+        );
+
+        $input = $request->validated();
+
+        $categorie = Categories::insert($input);
+    }
+}
